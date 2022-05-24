3f0b0fd15089f149bef367c89516005b17866828
Cruziper <goncalo.garcia.cruz@tecnico.ulisboa.pt>
2022-05-24 15:17:00 +0100

    Dispenser status implemented

--- ChessMain.py
+++ ChessMain.py
@@ -6,7 +6,7 @@ DIMENSION = 20
 MAX_FPS = 20
 SQUARE_SIZE = HEIGHT // DIMENSION
 
-dispensers = [(15,4), (4,4), (4,15), (15,15)]
+dispensers = [(4,4), (4,15), (15,4), (15,15)]
 
 NMBR_START_APPLES = 15
 NMBR_START_BANANAS = 10
@@ -16,6 +16,13 @@ apple = p.image.load('snake_imgs/apple.svg')
 banana = p.image.load('snake_imgs/banana.svg')
 strawberry = p.image.load('snake_imgs/strawberry.svg')
 
+# Dispenser States : 1 - ACTIVE , 0 - ON COOLDOWN
+TL_DISPENSER_STATE = 0  # top-left dispenser state
+TR_DISPENSER_STATE = 1  # top-right dispenser state
+BL_DISPENSER_STATE = 1  # bottom-left dispenser state
+BR_DISPENSER_STATE = 0  # bottom-right dispenser state
+
+cooldown = p.image.load('snake_imgs/cooldown.svg')
 
 '''
 Main function. Handles initializing application and updating graphics
@@ -26,15 +33,19 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     running = True
-    spawned = False
+    food_spawned = False
 
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
-        if not spawned:
+        event = p.event.wait()
+        if event.type == p.KEYDOWN:
+            if event.key == p.K_s:
+                food_spawned = False
+        if not food_spawned:
             drawGameSate(screen)
-            spawned = True
+            food_spawned = True
         clock.tick(MAX_FPS)
         p.display.flip()
 
@@ -46,6 +57,7 @@ def drawGameSate(screen):
 
     drawBoard(screen)   # draw squares of the board
     loadFood(screen)    # spawn food on the board
+    updateDispenserState(screen)    # verifies if the the dispenser is ACTIVE or ON COOLDOWN
 
 
 '''
@@ -100,7 +112,20 @@ def loadFood(screen):
                 occupied.append((rand_X, rand_Y))
                 count_strawberries += 1
 
-
+def updateDispenserState (screen):
+    for index in range(len(dispensers)):
+        if index == 0:
+            if TL_DISPENSER_STATE == 1:
+                screen.blit(cooldown, (dispensers[index][0]*SQUARE_SIZE, dispensers[index][1]*SQUARE_SIZE))
+        if index == 1:
+            if TR_DISPENSER_STATE == 1:
+                screen.blit(cooldown, (dispensers[index][0]*SQUARE_SIZE, dispensers[index][1]*SQUARE_SIZE))
+        if index == 2:
+            if BL_DISPENSER_STATE == 1:
+                screen.blit(cooldown, (dispensers[index][0]*SQUARE_SIZE, dispensers[index][1]*SQUARE_SIZE))
+        if index == 3:
+            if BR_DISPENSER_STATE == 1:
+                screen.blit(cooldown, (dispensers[index][0]*SQUARE_SIZE, dispensers[index][1]*SQUARE_SIZE))
 
 if __name__ == "__main__":
     main()
