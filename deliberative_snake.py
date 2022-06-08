695aabed0049116e2f3c5793fe630b7128239119
Cruziper <goncalo.garcia.cruz@tecnico.ulisboa.pt>
2022-06-08 07:50:24 +0100

    Circular scan area implemented

--- deliberative_snake.py
+++ deliberative_snake.py
@@ -1,14 +1,20 @@
 import pygame as p
+import numpy as np
 from pygame import Vector2
 from board import *
+
+VISION_RANGE = 5
 
 class Deliberative_Snake:
 
     def __init__(self):
         self.color = "olivedrab"
+        self.scanColor = "lemonchiffon3"
+        self.visibleArea = []
         
         self.body = [Vector2(7,10), Vector2(6,10), Vector2(5,10)]
         self.direction = p.Vector2(1, 0)
+        self.size = len(self.body)
         # TODO: rearrange which snake goes where in the start
         self.globalScore = 0
 
@@ -43,3 +49,44 @@ class Deliberative_Snake:
         body_copy = self.body[:]
         body_copy.append(body_copy[-1])
         self.body = body_copy[:]
+
+    def manhattanDistance (self, start: Vector2, end: Vector2):
+        return np.abs(end.x - start.x) + np.abs(end.y - start.y) 
+
+    def scanArea(self, screen):
+        self.circleBres(self.body[0].x, self.body[0].y, screen)
+        for i in range (DIMENSION):
+            for j in range (DIMENSION):
+                pos = p.Vector2(i,j)
+                if np.round(self.manhattanDistance(self.body[0], pos), 0) <= VISION_RANGE and pos not in self.body and pos not in self.visibleArea:
+                    self.visibleArea.append(pos)
+        self.paintVision(screen)
+
+    def paintVision(self, screen):
+        for cell in self.visibleArea:
+            p.draw.rect(screen, self.scanColor, p.Rect(cell.x * SQUARE_SIZE, cell.y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+    def drawCircle(self, xc: int, yc: int, x: int, y: int):
+        self.visibleArea.append(p.Vector2(xc+x, yc+y))
+        self.visibleArea.append(p.Vector2(xc-x, yc+y))
+        self.visibleArea.append(p.Vector2(xc+x, yc-y))
+        self.visibleArea.append(p.Vector2(xc-x, yc-y))
+        self.visibleArea.append(p.Vector2(xc+y, yc+x))
+        self.visibleArea.append(p.Vector2(xc-y, yc+x))
+        self.visibleArea.append(p.Vector2(xc+y, yc-x))
+        self.visibleArea.append(p.Vector2(xc-y, yc-x))
+
+    def circleBres(self, xc: int, yc: int, screen):
+        self.visibleArea = []
+        x = 0
+        y = VISION_RANGE
+        d = 3 - 2 * VISION_RANGE
+        self.drawCircle(xc, yc, x, y)
+        while y >= x:
+            x += 1
+            if (d > 0):
+                y -= 1
+                d = d + 4 * (x - y) + 10
+            else:
+                d = d + 4 * x + 6;
+            self.drawCircle(xc, yc, x, y)
--- snakeRoyale.py
+++ snakeRoyale.py
@@ -1,9 +1,8 @@
-from time import time
 import pygame as p
 from pygame import Vector2
 import sys
 from board import *
-from reactive_snake import *
+from deliberative_snake import *
 from dispenser_snake import *
 from trap_snake import *
 from deliberative_snake import *
@@ -22,8 +21,8 @@ def main():
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     screen.fill(p.Color("black"))
+    screen_color = "lemonchiffon1"
     running = True
-    food_spawned = False
 
     ## Generate all items
     board = Board()
@@ -35,13 +34,14 @@ def main():
 
     ## Create Snake
     # deliberative_snake : olivedrab,
-    # reactive_snake : orange2,
+    # deliberative_snake : orange2,
     # trap_snake : lightslategrey,
     # dispenser_snake : indianred
-    reactive_snake = Reactive_Snake()
+    deliberative_snake = Deliberative_Snake()
     
-    screen.fill("lemonchiffon1")
-    reactive_snake.drawSnake(screen)
+    screen.fill(screen_color)
+    deliberative_snake.drawSnake(screen)
+    deliberative_snake.scanArea(screen)
     fruits.drawFruits(screen)
     traps.drawTraps(screen)
     dispensers.drawDispensers(screen)
@@ -57,88 +57,89 @@ def main():
                 print("----------------------------------------------------------")
                 print("----------------------- SCORE BOARD ----------------------")
                 print("----------------------------------------------------------")
-                print("Global Score: ", reactive_snake.globalScore)
-                print("Snake Size: ", reactive_snake.size)
-                print("Apples: ", reactive_snake.apples, " x2 points")
-                print("Bananas: ", reactive_snake.bananas, " x3 points")
-                print("Strawberries: ", reactive_snake.strawberries, " x5 points")
-                print("Mushrooms: ", reactive_snake.mushrooms, "x(-1) points")
-                print("Ice: ", reactive_snake.ices)
-                print("Dispenser: ", reactive_snake.dispenser)
+                print("Global Score: ", deliberative_snake.globalScore)
+                print("Snake Size: ", deliberative_snake.size)
+                print("Apples: ", deliberative_snake.apples, " x2 points")
+                print("Bananas: ", deliberative_snake.bananas, " x3 points")
+                print("Strawberries: ", deliberative_snake.strawberries, " x5 points")
+                print("Mushrooms: ", deliberative_snake.mushrooms, "x(-1) points")
+                print("Ice: ", deliberative_snake.ices)
+                print("Dispenser: ", deliberative_snake.dispenser)
                 print("----------------------------------------------------------")
                 p.quit()
                 sys.exit()
             if e.type == SCREEN_UPDATE:
-                reactive_snake.moveSnake()
+                pass
+                deliberative_snake.moveSnake()
             if e.type == p.KEYDOWN:
-                if e.key == p.K_UP and reactive_snake.direction != Vector2(0,1):
-                    reactive_snake.direction = Vector2(0,-1)
-                if e.key == p.K_DOWN and reactive_snake.direction != Vector2(0,-1):
-                    reactive_snake.direction = Vector2(0,1)
-                if e.key == p.K_LEFT and reactive_snake.direction != Vector2(1,0):
-                    reactive_snake.direction = Vector2(-1,0)
-                if e.key == p.K_RIGHT and reactive_snake.direction != Vector2(-1,0):
-                    reactive_snake.direction = Vector2(1,0)
-        if reactive_snake.body[0] in fruits.apples:
+                if e.key == p.K_UP and deliberative_snake.direction != Vector2(0,1):
+                    deliberative_snake.direction = Vector2(0,-1)
+                if e.key == p.K_DOWN and deliberative_snake.direction != Vector2(0,-1):
+                    deliberative_snake.direction = Vector2(0,1)
+                if e.key == p.K_LEFT and deliberative_snake.direction != Vector2(1,0):
+                    deliberative_snake.direction = Vector2(-1,0)
+                if e.key == p.K_RIGHT and deliberative_snake.direction != Vector2(-1,0):
+                    deliberative_snake.direction = Vector2(1,0)
+        if deliberative_snake.body[0] in fruits.apples:
             # snake caught an apple
             # print("APPLE!!!")
             # delete APPLE from the board and fruits class
-            fruits.apples.remove(reactive_snake.body[0])
-            board.busy_cells.remove(reactive_snake.body[0])
+            fruits.apples.remove(deliberative_snake.body[0])
+            board.busy_cells.remove(deliberative_snake.body[0])
             # increase snake size
-            reactive_snake.increaseSize()
+            deliberative_snake.increaseSize()
             # add points
-            reactive_snake.apples += 1
-            reactive_snake.globalScore += 2
+            deliberative_snake.apples += 1
+            deliberative_snake.globalScore += 2
         
-        if reactive_snake.body[0] in fruits.bananas:
+        if deliberative_snake.body[0] in fruits.bananas:
             # snake caught a banana
             # print("BANANA!!!")
             # delete APPLE from the board and fruits class
-            fruits.bananas.remove(reactive_snake.body[0])
-            board.busy_cells.remove(reactive_snake.body[0])
+            fruits.bananas.remove(deliberative_snake.body[0])
+            board.busy_cells.remove(deliberative_snake.body[0])
             # increase snake size
-            reactive_snake.increaseSize()
+            deliberative_snake.increaseSize()
             # add points
-            reactive_snake.bananas += 1
-            reactive_snake.globalScore += 3
+            deliberative_snake.bananas += 1
+            deliberative_snake.globalScore += 3
             
-        if reactive_snake.body[0] in fruits.strawberries:
+        if deliberative_snake.body[0] in fruits.strawberries:
             # snake caught a strawberry
             # print("STRAWBERRY!!!")
             # delete APPLE from the board and fruits class
-            fruits.strawberries.remove(reactive_snake.body[0])
-            board.busy_cells.remove(reactive_snake.body[0])
+            fruits.strawberries.remove(deliberative_snake.body[0])
+            board.busy_cells.remove(deliberative_snake.body[0])
             # increase snake size
-            reactive_snake.increaseSize()
+            deliberative_snake.increaseSize()
             # add points
-            reactive_snake.strawberries += 1
-            reactive_snake.globalScore += 5
+            deliberative_snake.strawberries += 1
+            deliberative_snake.globalScore += 5
             
-        if reactive_snake.body[0] in traps.mushrooms:
+        if deliberative_snake.body[0] in traps.mushrooms:
             # snake caught a mushroom
             # print("MUSHROOM >__<")
             # delete APPLE from the board and fruits class
-            traps.mushrooms.remove(reactive_snake.body[0])
-            board.busy_cells.remove(reactive_snake.body[0])
+            traps.mushrooms.remove(deliberative_snake.body[0])
+            board.busy_cells.remove(deliberative_snake.body[0])
             # add points
-            reactive_snake.mushrooms += 1
-            reactive_snake.globalScore -= 1
+            deliberative_snake.mushrooms += 1
+            deliberative_snake.globalScore -= 1
             # TODO: Implement the reduction of points from other snakes (-4)
             
-        if reactive_snake.body[0] in traps.ices:
+        if deliberative_snake.body[0] in traps.ices:
             # snake caught an ice
             # print("ICE *__*")
             # delete APPLE from the board and fruits class
-            traps.ices.remove(reactive_snake.body[0])
-            board.busy_cells.remove(reactive_snake.body[0])
+            traps.ices.remove(deliberative_snake.body[0])
+            board.busy_cells.remove(deliberative_snake.body[0])
             # add points
-            reactive_snake.ices += 1
+            deliberative_snake.ices += 1
             
-        if reactive_snake.body[0] in dispensers.dispensers:
+        if deliberative_snake.body[0] in dispensers.dispensers:
             # print("DISPENSER :P")
             for dispenserPos in dispensers.dispensers:
-                if reactive_snake.body[0] == dispenserPos and dispensers.STATE == 0:
+                if deliberative_snake.body[0] == dispenserPos and dispensers.STATE == 0:
                     dispensers.STATE = 1
                     dispenserTimer = p.time.get_ticks()
         
@@ -147,56 +148,57 @@ def main():
             if dispensers.STATE == 1 and p.time.get_ticks() - dispenserTimer >= 5000:
                 fruits.definePositionsDispenser(board)
                 dispensers.STATE = 2
-                reactive_snake.dispenser += 1
+                deliberative_snake.dispenser += 1
                 # TODO: Assuming there is only one snake on the board // Add if to check which snakes have the propertie REWARD at true, divide the points and distribute to each one
-                reactive_snake.globalScore += 8
+                deliberative_snake.globalScore += 8
                 dispenserCooldown = p.time.get_ticks()
             
             if dispensers.STATE == 2 and p.time.get_ticks() - dispenserCooldown >= 5000:
                 dispensers.STATE = 0
 
         
-        if reactive_snake.body[0] in reactive_snake.body[1:] or reactive_snake.body[0].x < 0 or reactive_snake.body[0].x >= board.boardSize or reactive_snake.body[0].y < 0 or reactive_snake.body[0].y >= board.boardSize:
+        if deliberative_snake.body[0] in deliberative_snake.body[1:] or deliberative_snake.body[0].x < 0 or deliberative_snake.body[0].x >= board.boardSize or deliberative_snake.body[0].y < 0 or deliberative_snake.body[0].y >= board.boardSize:
             # snake hit itself or went off the edges
             print("\n YOU LOST !\n")
             print("----------------------------------------------------------")
             print("----------------------- SCORE BOARD ----------------------")
             print("----------------------------------------------------------")
-            print("Global Score: ", reactive_snake.globalScore)
-            print("Snake Size: ", reactive_snake.size)
-            print("Apples: ", reactive_snake.apples, " x2 points")
-            print("Bananas: ", reactive_snake.bananas, " x3 points")
-            print("Strawberries: ", reactive_snake.strawberries, " x5 points")
-            print("Mushrooms: ", reactive_snake.mushrooms, "x(-1) points")
-            print("Ice: ", reactive_snake.ices)
-            print("Dispenser: ", reactive_snake.dispenser)
+            print("Global Score: ", deliberative_snake.globalScore)
+            print("Snake Size: ", deliberative_snake.size)
+            print("Apples: ", deliberative_snake.apples, " x2 points")
+            print("Bananas: ", deliberative_snake.bananas, " x3 points")
+            print("Strawberries: ", deliberative_snake.strawberries, " x5 points")
+            print("Mushrooms: ", deliberative_snake.mushrooms, "x(-1) points")
+            print("Ice: ", deliberative_snake.ices)
+            print("Dispenser: ", deliberative_snake.dispenser)
             print("----------------------------------------------------------")
             running = False
 
-        elif reactive_snake.size == 40 or reactive_snake.globalScore >= 100:
+        elif deliberative_snake.size == 40 or deliberative_snake.globalScore >= 100:
             # snake achieved the maximum size or points and WON!
             print("\n YOU WIN !\n")
             print("----------------------------------------------------------")
             print("----------------------- SCORE BOARD ----------------------")
             print("----------------------------------------------------------")
-            print("Global Score: ", reactive_snake.globalScore)
-            print("Snake Size: ", reactive_snake.size)
-            print("Apples: ", reactive_snake.apples, " x2 points")
-            print("Bananas: ", reactive_snake.bananas, " x3 points")
-            print("Strawberries: ", reactive_snake.strawberries, " x5 points")
-            print("Mushrooms: ", reactive_snake.mushrooms, "x(-1) points")
-            print("Ice: ", reactive_snake.ices)
-            print("Dispenser: ", reactive_snake.dispenser)
+            print("Global Score: ", deliberative_snake.globalScore)
+            print("Snake Size: ", deliberative_snake.size)
+            print("Apples: ", deliberative_snake.apples, " x2 points")
+            print("Bananas: ", deliberative_snake.bananas, " x3 points")
+            print("Strawberries: ", deliberative_snake.strawberries, " x5 points")
+            print("Mushrooms: ", deliberative_snake.mushrooms, "x(-1) points")
+            print("Ice: ", deliberative_snake.ices)
+            print("Dispenser: ", deliberative_snake.dispenser)
             print("----------------------------------------------------------")
             running = False
 
         screen.fill("lemonchiffon1")
+        deliberative_snake.scanArea(screen)
         fruits.drawFruits(screen)
         traps.drawTraps(screen)
         dispensers.drawDispensers(screen)
         dispensers.updateDispenserState(screen)
         #TODO: Implement the cooldown factor on the dispensers
-        reactive_snake.drawSnake(screen)
+        deliberative_snake.drawSnake(screen)
         board.drawLines(screen)
         p.display.update()
         clock.tick(MAX_FPS)
